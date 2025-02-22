2e2d001eb362fce7ec35ea107676ff7387809fd3
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-02-22 19:19:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # api key: e6bcf177-33ab-4d68-87ba-f2ab030011c5
 import requests
-json_data = requests.get('https://api.rasp.yandex.net/v3.0/search/?apikey=e6bcf177-33ab-4d68-87ba-f2ab030011c5&format=json&from=s9610291&to=s9610287&page=1&date=2025-02-22').json()
-print(json_data)
+import json
+
+schedul = requests.get(f'https://api.rasp.yandex.net/v3.0/search/?apikey=e6bcf177-33ab-4d68-87ba-f2ab030011c5&format=json&from=s{9610291}&to=s{9610287}&page=1&date=2025-02-22').json()
+print(schedul)
+dep_time = []
+for i in schedul:
+    for train in schedul['segments']:
+        dep_time.append(train['departure'][11:16])
+        print(train['stops'], train['departure'][11:16], train['duration']/60, train['start_date'])
+        if (dep_time[0] == train['departure'][11:16]) and (len(dep_time) > 1): break
+
+
 
 def station_code ():
     '''
