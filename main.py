e11aae7b201362b0e80697ea18bf518bea1cf038
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-03-02 01:05:59 +0700

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import requests
 import datetime
 from ics import Calendar, Event
+from urllib3.filepost import choose_boundary
+
 c = Calendar()
 
 
@@ -10,16 +12,23 @@ station_dict = {1:'9610291',
                 3:'9610192',
                 4:'9610287',
                 5:'9610219'}
+back_station_dict = {1: 'Обское море',
+                     2: 'Сеятель',
+                     3: 'Новосибирск-южный',
+                     4: 'Речной вокзал',
+                     5: 'Камышенская'}
 
 dep_time = []
 arr_time = []
 train_date = []
 train_duration = []
+trip = {}
 def main():
     global dep_time
     global arr_time
     global train_date
     global train_duration
+    global arr
     dep = 0
     arr = 0
     while dep == arr:
@@ -33,38 +42,38 @@ def main():
 -->'''))
         arr_input = int(input('''
 Выберите станцию прибытия:
-        1) Обское море
-        2)Сеятель
-        3) Новосибирск-южный
-        4) Речной вокзал
-        5) Камышенская
-    -->'''))
+    1) Обское море
+    2) Сеятель
+    3) Новосибирск-южный
+    4) Речной вокзал
+    5) Камышенская
+-->'''))
         dep = station_dict.get(dep_input)
         arr = station_dict.get(arr_input)
 
     schedul = requests.get(f'https://api.rasp.yandex.net/v3.0/search/?apikey=e6bcf177-33ab-4d68-87ba-f2ab030011c5&format=json&from=s{dep}&to=s{arr}&page=1&date={datetime.date.today().isoformat()}').json()
     print(schedul)
 
+
     for i in schedul:
         for train in schedul['segments']:
+            trip[train['departure'][11:16]] = ([train['arrival'][11:16], train['duration']/60])
             dep_time.append(train['departure'][11:16])
-            arr_time.append(train['arrival'][11:16])
-            train_date.append(train['start_date'])
-            train_duration.append(int(train['duration']/60))
-            #print(train['departure'][11:16], train['duration']/60, train['start_date'])
             if (dep_time[0] == train['departure'][11:16]) and (len(dep_time) > 1): break
+    print(f'От станции {back_station_dict.get(dep_input)} до станции {back_station_dict.get(arr_input)} '
+          f'найдены следующие поезда:')
+    for k in trip.keys():
+        print(f'Отправления: {k}, Прибытие: {trip[k][0]}, Время в пути: {trip[k][1]}')
+    choose_train = ''
+    while choose_train not in trip.keys():
+        choose_train = input('Выбирете время отправления -->')
+    print(choose_train)
 
-    dep_time = dep_time[0:len(dep_time)-3]
-    arr_time = arr_time[0:len(arr_time)-3]
-    train_date = train_date[0:len(train_date)-3]
-    train_duration = train_duration[0:len(train_duration)-3]
+    create_event(f'{datetime.date.today().isoformat()}', choose_train, f'{trip[choose_train][0]}', f'{back_station_dict.get(arr_input)}')
 
-    for z in range(0, len(dep_time)):
-        create_event(z, f'{train_date[z]}', f'{dep_time[z]}', f'{arr_time[z]}')
-
-def create_event(number, date, dep, arr):
+def create_event(date, dep, arr, st_arr):
     name = Event()
-    name.name = f'Электричка на {dep}'
+    name.name = f'Электричка на {st_arr}'
     corrected_dep = datetime.datetime.strptime(f'{date} {dep}:00', '%Y-%m-%d %H:%M:%S') - datetime.timedelta(hours=7)
     corrected_arr = datetime.datetime.strptime(f'{date} {arr}:00', '%Y-%m-%d %H:%M:%S') - datetime.timedelta(hours=7)
     begin = corrected_dep.strftime('%Y-%m-%d %H:%M:%S')
@@ -73,7 +82,7 @@ def create_event(number, date, dep, arr):
     name.end = f'{end}'
     c.events.add(name)
     c.events
-    with open('my.ics', 'w') as my_file:
+    with open('train.ics', 'w') as my_file:
         my_file.writelines(c.serialize_iter())
 
 def station_code ():
